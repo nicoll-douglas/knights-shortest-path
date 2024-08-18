b8ff6e2b966d1778de2b1541dd79af54bd88164c
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-08-18 13:50:18 +0100

    add comments

--- src/lib/getPossibleKnightMoves.js
+++ src/lib/getPossibleKnightMoves.js
@@ -1,4 +1,4 @@
-// possible steps that would represent knight moves if "added" to a position
+// step vectors a knight can take
 const STEPS = [
   [1, 2],
   [2, 1],
@@ -10,16 +10,15 @@ const STEPS = [
   [-1, 2],
 ];
 
-// we model positions on the board with an array [x, y]
-// x and y being in the range 0-7
 export default function getPossibleKnightMoves(position) {
+  // position of knight on the board, coord range is 0-7
   const [x, y] = position;
   const moves = [];
   for (let i = 0; i < STEPS.length; i++) {
     const [dx, dy] = STEPS[i];
     const move = [x + dx, y + dy];
-    // check for each step whether the position is a valid move on the board (and not off of it)
     if (move[0] <= 7 && move[1] <= 7 && move[0] >= 0 && move[1] >= 0) {
+      // if on board, push to moves
       moves.push(move);
     }
   }
--- src/lib/shortestNumberOfKnightMoves.js
+++ src/lib/shortestNumberOfKnightMoves.js
@@ -4,34 +4,35 @@ export default function shortestNumberOfKnightMoves(
   initialPosition,
   targetPosition
 ) {
-  const movesCounter = {};
-  movesCounter[initialPosition.toString()] = 0;
-  const pathRecorder = {};
-  pathRecorder[initialPosition.toString()] = [initialPosition];
+  const movesCounter = {}; // initialise move counter
+  movesCounter[initialPosition.toString()] = 0; // initial to initial is 0
+  const pathRecorder = {}; // initialise object to record path to target
+  pathRecorder[initialPosition.toString()] = [initialPosition]; // path starts from initialPosition
 
-  const queue = [initialPosition];
+  const queue = [initialPosition]; // queue to simulate breadth-first search
+  // check if current position is the target
   while (
     !(queue[0][0] === targetPosition[0] && queue[0][1] === targetPosition[1])
   ) {
-    const currentPosition = queue.shift();
-    const possibleMoves = getPossibleKnightMoves(currentPosition);
+    const currentPosition = queue.shift(); // remove from queue since it isn't
+    const possibleMoves = getPossibleKnightMoves(currentPosition); // get possible moves
 
     possibleMoves.forEach((move) => {
-      if (move.toString() in movesCounter) return;
-      queue.push(move);
+      if (move.toString() in movesCounter) return; // means move was traversed, so return since shorter path was found
+      queue.push(move); // push move to queue to be examined whether it is the target
 
       movesCounter[move.toString()] =
-        movesCounter[currentPosition.toString()] + 1;
+        movesCounter[currentPosition.toString()] + 1; // update move counter, no. of moves to possible is current plus 1
 
       pathRecorder[move.toString()] = [
         ...pathRecorder[currentPosition.toString()],
         move,
-      ];
+      ]; // update path record, path to possible move is path to current + position of move
     });
   }
 
   return {
     moves: movesCounter[targetPosition.toString()],
     path: pathRecorder[targetPosition.toString()],
-  };
+  }; // when loop finishes traversing, target position data is registered with shortest in mind
 }
